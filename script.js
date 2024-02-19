02ce0a4c13c34c4604200f35e236c165ebd15092
Varja <122620971+VarjaBerglund@users.noreply.github.com>
2024-02-19 13:40:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    const eventDate = "06/22/2024";
+    
+    const countDown = new Date(eventDate).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+          // Gör något när datumet är nått
+          if (distance < 0) {
+            document.getElementById("headline").innerText = "Filmen är här!";
+            document.getElementById("countdown").style.display = "none";
+            clearInterval(x);
+          }
+        }, 0)
+}());
